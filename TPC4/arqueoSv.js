7c473a4dbb2c4e135125cdc44ddaeceb3b394c58
gabri <8dgabrielsantos@gmail.com>
2019-10-08 19:04:12 +0000

    TPC4

---
+++ TPC4/arqueoSv.js
@@ -0,0 +1,20 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function (req, res) {
+    var partes = req.url.split('/')
+    var file = partes[partes.length - 1]
+    console.log(file)
+    if (file % 2 == 0 && file <= 244) {
+        fs.readFile('files/arq' + file + '.xml', function (err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/xml' })
+            res.write(data)
+            res.end
+        })
+
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write("File inexistente: " + file)
+        res.end
+    }
+}).listen(7777)
